f5aa6f4ae5b4fbfb3e46e7b16cef947b3e523ed8
Steven Wen <73114262+StevenWen11@users.noreply.github.com>
2021-05-19 14:32:26 +0700

    CodeChef Practice(Easy)

---
+++ Easy/EvenSum.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for i in range(t):
+    length_arr = int(input())
+    arr = list(map(int, input().split()))
+    add = 0
+    
+    for i in arr:
+        add += i
+        
+    if add%2 == 1:
+        print(2)
+    else:
+        print(1)
