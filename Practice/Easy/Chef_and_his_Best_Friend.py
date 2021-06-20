0ab9dd147bf464290b5a53770e23d82552bb6ec7
Steven Wen <73114262+StevenWen81@users.noreply.github.com>
2021-06-20 13:59:26 +0700

    Delete Chef_and_his_Best_Friend.py

--- Practice/Easy/Chef_and_his_Best_Friend.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for i in range(t):
-    x = int(input())
-    arr = list(map(int, input().split()))
-    zero = arr.count(0)
-    #print(zero)
-    ans = x - zero
-    ind = arr.index(0)
-    print(ans - ind)
